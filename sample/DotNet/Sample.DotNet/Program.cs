28d82c7e14be554d1414540d569db78191610b24
MihailPw <m.podshivadlov@gmail.com>
2019-08-18 16:28:19 +0300

    Add sample;

---
+++ sample/DotNet/Sample.DotNet/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Sample.DotNet.Api;
+
+namespace Sample.DotNet
+{
+    internal class Program
+    {
+        private const string GraphQlUrl = "https://swapi.apis.guru";
+
+
+        public static async Task Main(string[] args)
+        {
+            var clientFactory = new GraphQlApiClientFactory(GraphQlUrl);
+
+            var data = await clientFactory.CreateQueryClient(r => r
+                .AllFilms()(fc => fc
+                    .TotalCount()
+                    .Films()(f => f
+                        .Director()
+                        .Created()
+                        .Id()
+                        .Title())))
+                .SendAsync();
+
+            WriteJsonToConsole(data);
+            Console.ReadLine();
+        }
+
+
+        private static void WriteJsonToConsole(object value)
+        {
+            Console.WriteLine(JsonConvert.SerializeObject(value, Formatting.Indented, new JsonSerializerSettings
+            {
+                NullValueHandling = NullValueHandling.Ignore,
+            }));
+        }
+    }
+}
